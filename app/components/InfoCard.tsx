8ba99b25fffd8e20fd069f013ea5cf2fa81d7b9a
Shawn D'silva <shawn.dsilva_97@outlook.com>
2025-10-30 11:26:02 +0000

    InfoCard styling

--- app/components/InfoCard.tsx
+++ app/components/InfoCard.tsx
@@ -7,19 +7,25 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-const InfoCard = () => {
+import { LucideArrowBigRight, LucideArrowRight } from "lucide-react"
+import Link from "next/link"
+const InfoCard = ({ data }) => {
     return (
-        <Card className="w-1/4 rounded-none">
-            <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-                <CardAction>Card Action</CardAction>
+        <Card className="w-1/4 rounded-none grid grid-rows-8 gap-2">
+            <CardHeader className="row-span-4">
+                <CardTitle className="text-2xl font-medium row-span-1!">{data?.title}</CardTitle>
+                <hr className="row-span-1!"></hr>
+                <CardDescription className="text-lg text-gray-400 row-span-1!">{data?.desc}</CardDescription>
             </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
+            <CardContent className="row-span-2">
+                <p className="text-4xl  text-blue-900">{data?.value}</p>
             </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
+            <CardFooter className="row-span-1">
+                <Link className="text-2xl pb-2 text-blue-600 hover:text-blue-900 hover:border-b-2 border-blue-900" href={""}>
+                    <p>
+                        View Details <LucideArrowRight className="inline-block" />
+                    </p>
+                </Link>
             </CardFooter>
         </Card>
     )
--- app/components/MyCardDrawer.tsx
+++ app/components/MyCardDrawer.tsx
@@ -4,9 +4,29 @@ import { ChevronDown, ChevronUp } from 'lucide-react'
 import React, { useState } from 'react'
 import InfoCard from './InfoCard'
 
+const INFO = [
+    {
+        title: "Water Availability and Usage Analysis",
+        desc: "Includes data on evapotranspiration, precipitation, water footprint, virtual water, and water productivity.",
+        value: "154.17",
+    }, {
+        title: "Ecosystem and Land Insights",
+        desc: "Provides information on biomass production, land cover classification, and hydronomic zones.",
+        value: "154.17",
+    }, {
+        title: "Climate and Environmental Conditions",
+        desc: "Offers data related to drought conditions and climate change.",
+        value: " 154.17",
+    }, {
+        title: "Additional Data and Resources",
+        desc: "Contains data on cropping intensity, elevation, population, and surface water.",
+        value: " 154.17",
+    },
+]
+
+
 const MyCardDrawer = ({ children }) => {
     const [toggle, setToggle] = useState(false)
-
 
     return (
         <div className='w-full absolute bottom-0 left-0 '>
@@ -14,7 +34,7 @@ const MyCardDrawer = ({ children }) => {
                 {toggle ? <ChevronDown /> : <ChevronUp />}
             </Button>
             <div className={`w-full bg-gray-500/60 duration-300 ease-linear ${toggle ? "h-[500px] p-10" : "h-0"}  w-full flex flex-row gap-8`}>
-                {toggle && [...Array(4)].map((item, index) => <InfoCard key={index} item={item} />)}
+                {toggle && INFO.map((item, index) => <InfoCard key={index} data={item} />)}
             </div>
         </div>
     )
