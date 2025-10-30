572745b3fa67da045cfac03b932372a966f012f8
Shawn D'silva <shawn.dsilva_97@outlook.com>
2025-10-30 13:11:23 +0000

    Total and Cropland toggle added

--- app/components/InfoCard.tsx
+++ app/components/InfoCard.tsx
@@ -11,7 +11,7 @@ import { LucideArrowBigRight, LucideArrowRight } from "lucide-react"
 import Link from "next/link"
 const InfoCard = ({ data }) => {
     return (
-        <Card className="w-1/4 rounded-none grid grid-rows-8 gap-2">
+        <Card className="w-1/4! rounded-none grid grid-rows-8 gap-2">
             <CardHeader className="row-span-4">
                 <CardTitle className="text-2xl font-medium row-span-1!">{data?.title}</CardTitle>
                 <hr className="row-span-1!"></hr>
@@ -21,7 +21,7 @@ const InfoCard = ({ data }) => {
                 <p className="text-4xl  text-blue-900">{data?.value}</p>
             </CardContent>
             <CardFooter className="row-span-1">
-                <Link className="text-2xl pb-2 text-blue-600 hover:text-blue-900 hover:border-b-2 border-blue-900" href={""}>
+                <Link className="text-2xl pb-2 text-blue-600 hover:text-blue-900 border-b-2 border-transparent hover:border-blue-900" href={""}>
                     <p>
                         View Details <LucideArrowRight className="inline-block" />
                     </p>
--- app/components/MyCardDrawer.tsx
+++ app/components/MyCardDrawer.tsx
@@ -3,38 +3,63 @@ import { Button } from '@/components/ui/button'
 import { ChevronDown, ChevronUp } from 'lucide-react'
 import React, { useState } from 'react'
 import InfoCard from './InfoCard'
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Separator } from "@/components/ui/separator"
 const INFO = [
     {
-        title: "Water Availability and Usage Analysis",
+        title: "Water Availability & Usage",
         desc: "Includes data on evapotranspiration, precipitation, water footprint, virtual water, and water productivity.",
         value: "154.17",
     }, {
-        title: "Ecosystem and Land Insights",
+        title: "Ecosystem & Land Insights",
         desc: "Provides information on biomass production, land cover classification, and hydronomic zones.",
         value: "154.17",
     }, {
-        title: "Climate and Environmental Conditions",
+        title: "Climate & Environmental Conditions",
         desc: "Offers data related to drought conditions and climate change.",
         value: " 154.17",
     }, {
-        title: "Additional Data and Resources",
+        title: "Additional Data & Resources",
         desc: "Contains data on cropping intensity, elevation, population, and surface water.",
         value: " 154.17",
     },
 ]
 
 
+
+
 const MyCardDrawer = ({ children }) => {
     const [toggle, setToggle] = useState(false)
+    const [selectedStats, setSelectedStats] = useState("total")
 
     return (
         <div className='w-full absolute bottom-0 left-0 '>
             <Button variant={"ghost"} size={"lg"} className='bg-gray-500/60 rounded-none rounded-t-lg mx-auto md:ml-3 hover:bg-gray-500/40 text-white *:size-7! cursor-pointer' onClick={() => setToggle(!toggle)}>
                 {toggle ? <ChevronDown /> : <ChevronUp />}
             </Button>
-            <div className={`w-full bg-gray-500/60 duration-300 ease-linear ${toggle ? "h-[500px] p-10" : "h-0"}  w-full flex flex-row gap-8`}>
-                {toggle && INFO.map((item, index) => <InfoCard key={index} data={item} />)}
+            <div className={`w-full bg-gray-500/60 duration-300 ease-linear ${toggle ? "h-1/2 p-10" : "h-0"} `}>
+
+                {/* <Button className={selectedStats === "total" ? "bg-blue-900 text-white" : ""} variant={"outline"} onClick={() => setSelectedStats("total")}>Total Area Stats</Button>
+                <Button className={selectedStats === "cropland" ? "bg-blue-900 text-white" : ""} variant={"outline"} onClick={() => setSelectedStats("cropland")}>Cropland Area Stats</Button> */}
+                <div className={!toggle ? "hidden" : ""}>
+                    <h1 className='text-2xl font-medium text-white py-2'>View Overall Stats</h1>
+                    <p className='text-lg font-light text-white pb-4'>Switch between Total and Cropland stats using the buttons below Click on the cards to view them.</p>
+
+                    <Tabs defaultValue="total" >
+                        <TabsList>
+                            <TabsTrigger value="total">Total Area Stats</TabsTrigger>
+                            <TabsTrigger value="cropland">Cropland Area Stats</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="total"></TabsContent>
+                        <TabsContent value="cropland"></TabsContent>
+                    </Tabs>
+
+                    <Separator className='my-4' />
+                </div>
+
+                <div className=' w-full flex flex-row gap-8'>
+                    {toggle && INFO.map((item, index) => <InfoCard key={index} data={item} />)}
+                </div>
             </div>
         </div>
     )
