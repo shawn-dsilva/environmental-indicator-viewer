f7b5128b707f4dbc9318361cae4892755648f30d
Shawn D'silva <shawn.dsilva_97@outlook.com>
2025-10-31 10:09:36 +0000

    basic chart layout done

--- app/components/LineChart.tsx
+++ app/components/LineChart.tsx
@@ -9,7 +9,7 @@ import {
     Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
+import { fa, faker } from '@faker-js/faker';
 
 ChartJS.register(
     CategoryScale,
@@ -23,13 +23,10 @@ ChartJS.register(
 
 export const options = {
     responsive: true,
+    maintainAspectRatio: false,
     plugins: {
         legend: {
-            position: 'top' as const,
-        },
-        title: {
-            display: true,
-            text: 'Chart.js Line Chart',
+            position: 'bottom' as const,
         },
     },
 };
@@ -40,13 +37,13 @@ export const data = {
     labels,
     datasets: [
         {
-            label: 'Dataset 1',
+            label: 'Evapotranspiration',
             data: labels.map(() => faker.number.float({ min: -1000, max: 1000 })),
             borderColor: 'rgb(255, 99, 132)',
             backgroundColor: 'rgba(255, 99, 132, 0.5)',
         },
         {
-            label: 'Dataset 2',
+            label: 'Precipitation',
             data: labels.map(() => faker.number.float({ min: -1000, max: 1000 })),
             borderColor: 'rgb(53, 162, 235)',
             backgroundColor: 'rgba(53, 162, 235, 0.5)',
@@ -55,5 +52,13 @@ export const data = {
 };
 
 export function LineChart() {
-    return <Line options={options} data={data} />;
+    return <div className='w-full rounded-lg border border-gray-400'>
+        <div className='bg-gray-200 p-4 rounded-t-lg border-b-gray-800'>
+            <h1>Evapotranspiration & Precipitation</h1>
+        </div>
+        <div className='p-3 m-auto w-full max-h-[380px]'>
+            <Line options={options} data={data} />
+        </div>
+    </div>
+
 }
